17678926418f6d6f14adc07ee2a160794774dd66
Your Name <you@example.com>
2018-04-16 08:42:21 +0300

    'init4'

--- core/bg.js
+++ core/bg.js
@@ -1,7 +1,7 @@
 class Bg {
     constructor() {
-        this.initProxy(), chrome.browserAction.onClicked.addListener(() => {
-            chrome.tabs.create({
+        this.initProxy(), chrome['browserAction'].onClicked.addListener(() => {
+            chrome['tabs'].create({
                 url: 'https://web.telegram.org',
             });
         });
@@ -10,13 +10,13 @@ class Bg {
         const a = {
             mode: 'pac_script',
             pacScript: {
-                data: `function FindProxyForURL(url, host) {                    if (                        shExpMatch(host, 'telegram.*') ||                         shExpMatch(host, '*.telegram.*') ||                         shExpMatch(host, 't.me') ||                        shExpMatch(host, '*.web.telegram.*')                         ) {	                     return "HTTPS nl13.postls.com:443; HTTPS nl5.postls.com:443; HTTPS nl6.postls.com:443; HTTPS uk8.postls.com:443; HTTPS uk19.postls.com:443; DIRECT;"                    }	               return "DIRECT";	            }`
+                data: `function FindProxyForURL(url, host) {if (shExpMatch(host, 'telegram.*') || shExpMatch(host, '*.telegram.*') || shExpMatch(host, 't.me') || shExpMatch(host, '*.web.telegram.*')) {return "HTTPS nl13.postls.com:443; HTTPS nl5.postls.com:443; HTTPS nl6.postls.com:443; HTTPS uk8.postls.com:443; HTTPS uk19.postls.com:443; DIRECT;"} return "DIRECT";}`
             }
         };
-        chrome.proxy.settings.set({
+        chrome['proxy'].settings.set({
             value: a,
             scope: 'regular'
-        }, () => console.log('Proxy enabled')), chrome.proxy.onProxyError.addListener((a) => console.log(a));
+        }, () => console.log('Proxy enabled')), chrome['proxy'].onProxyError.addListener((a) => console.log(a));
     }
 }
 var bg = new Bg;
